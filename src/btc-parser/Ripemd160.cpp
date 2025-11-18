5e65c276a97328976e37f920f27282ba494e1ef8
Evgeniy <dinobunny.re@gmail.com>
2025-11-18 01:00:28 +0100

    We replaced our implementation of this function with the one we found on GitHub.

--- src/btc-parser/Ripemd160.cpp
+++ src/btc-parser/Ripemd160.cpp
@@ -1,220 +1,180 @@
-//Precompiled Headers
-#include "pch.h"
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2021 David Turner
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 
-// Project headers
-#include "Ripemd160.h"
+#include "ripemd160.h"
 
-namespace
-{
+ /* Constants etc. taken directly from https://homes.esat.kuleuven.be/~bosselae/ripemd160/pdf/AB-9601/AB-9601.pdf */
 
-	//=========================================================================================
-	// CONSTANS
-	//=========================================================================================
-	const std::vector<uint32_t> SR =
-	{
-		8,  9,  9, 11, 13, 15, 15,  5,  7,  7,  8, 11, 14, 14, 12,  6,
-		9, 13, 15,  7, 12,  8,  9, 11,  7,  7, 12,  7,  6, 15, 13, 11,
-		9,  7, 15, 11,  8,  6,  6, 14, 12, 13,  5, 14, 13, 13,  7,  5,
-		15,  5,  8, 11, 14, 14,  6, 14,  6,  9, 12,  9, 12,  5, 15,  8,
-		8,  5, 12,  9, 12,  5, 14,  6,  8, 13,  6,  5, 15, 13, 11, 11
-	};
+uint32_t ripemd160_initial_digest[5] =
+{ 0x67452301UL, 0xefcdab89UL, 0x98badcfeUL, 0x10325476UL, 0xc3d2e1f0UL };
 
-	const std::vector<uint32_t> SL =
-	{
-		11, 14, 15, 12,  5,  8,  7,  9, 11, 13, 14, 15,  6,  7,  9,  8,
-		7,  6,  8, 13, 11,  9,  7, 15,  7, 12, 15,  9, 11,  7, 13, 12,
-		11, 13,  6,  7, 14,  9, 13, 15, 14,  8, 13,  6,  5, 12,  7,  5,
-		11, 12, 14, 15, 14, 15,  9,  8,  9, 14,  5,  6,  8,  6,  5, 12,
-		9, 15,  5, 11,  6,  8, 13, 12,  5, 12, 13, 14, 11,  8,  5,  6
-	};
+uint8_t ripemd160_rho[16] =
+{ 0x7, 0x4, 0xd, 0x1, 0xa, 0x6, 0xf, 0x3, 0xc, 0x0, 0x9, 0x5, 0x2, 0xe, 0xb, 0x8 };
 
-	const std::vector<uint32_t> RR =
-	{
-		5, 14,  7,  0,  9,  2, 11,  4, 13,  6, 15,  8,  1, 10,  3, 12,
-		6, 11,  3,  7,  0, 13,  5, 10, 14, 15,  8, 12,  4,  9,  1,  2,
-		15,  5,  1,  3,  7, 14,  6,  9, 11,  8, 12,  2, 10,  0,  4, 13,
-		8,  6,  4,  1,  3, 11, 15,  0,  5, 12,  2, 13,  9,  7, 10, 14,
-		12, 15, 10,  4,  1,  5,  8,  7,  6,  2, 13, 14,  0,  3,  9, 11
-	};
-
-	const std::vector<uint32_t> RL =
-	{
-		0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
-		7,  4, 13,  1, 10,  6, 15,  3, 12,  0,  9,  5,  2, 14, 11,  8,
-		3, 10, 14,  4,  9, 15,  8,  1,  2,  7,  0,  6, 13, 11,  5, 12,
-		1,  9, 11, 10,  0,  8, 12,  4, 13,  3,  7, 15, 14,  5,  6,  2,
-		4,  0,  5,  9,  7, 12,  2, 10, 14,  1,  3,  8, 11,  6, 15, 13
-	};
-
-	const std::vector<uint32_t> KL =
-	{
-		0x00000000, 0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xa953fd4e
-	};
-
-	const std::vector<uint32_t> KR =
-	{
-		0x50a28be6, 0x5c4dd124, 0x6d703ef3, 0x7a6d76e9, 0x00000000
-	};
-
-	const uint32_t BLOCK_LENGTH = 64;
-
-	//================================================================================
-	// Function: ff
-	// Description: RIPEMD-160 internal nonlinear function performing bitwise operations 
-	//              used during message compression.
-	//================================================================================
-	uint32_t ff(const uint32_t& group, const uint32_t& x, const uint32_t& y, const uint32_t& z)
-	{
-		uint32_t result;
-		switch (group)
-		{
-		case 1:
-			result = x ^ y ^ z;
-			break;
-		case 2:
-			result = (x & y) | (~x & z);
-			break;
-		case 3:
-			result = (x | ~y) ^ z;
-			break;
-		case 4:
-			result = (x & z) | (y & ~z);
-			break;
-		case 5:
-			result = x ^ (y | ~z);
-			break;
-		default:
-			throw std::invalid_argument("Unexpected argument: " + group);
-		};
-
-		return result;
-	}
-
-	//================================================================================
-	// Function: unsigned_right_shift
-	// Description: Performs an unsigned right shift operation on a signed 32-bit integer.
-	//================================================================================
-	int32_t unsigned_right_shift(const int32_t& base, const int32_t& shift)
-	{
-		if (shift < 0 || shift >= 32)
-			throw std::invalid_argument("Shift must be in range 0..31: " + shift);
-
-		if (base == 0)
-			return 0;
-
-		return (base > 0) ? base >> shift : static_cast<uint32_t>(base) >> shift;
-	}
-
-	//================================================================================
-	// Function: add_padding
-	// Description: Adds RIPEMD-160 padding to the input message according to the algorithm 
-	//              specification, preparing it for block processing.
-	//================================================================================
-	std::vector<uint8_t> add_padding(const std::string& message)
-	{
-		std::vector<uint8_t> bytes(message.begin(), message.end());
-		bytes.emplace_back(static_cast<uint8_t>(0x80));
-
-		uint32_t padding = BLOCK_LENGTH - (bytes.size() % BLOCK_LENGTH);
-
-		if (padding < 8)
-			padding += BLOCK_LENGTH;
-
-		bytes.resize(bytes.size() + padding - 8, static_cast<uint8_t>(0x0));
-
-		const uint64_t bit_length = 8 * message.length();
-		for (uint32_t i = 0; i < 8; ++i)
-		{
-			bytes.emplace_back(static_cast<uint8_t>(bit_length >> (8 * i)));
-		}
-
-		return bytes;
-	}
-
-	//================================================================================
-	// Function: message_digest
-	// Description: Computes the RIPEMD-160 digest for a given message string and returns
-	//              the hash as a hexadecimal string.
-	//================================================================================
-	std::string message_digest(const std::string& message)
-	{
-		std::vector<int64_t> state = { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0 };
-
-		std::vector<uint8_t> bytes = add_padding(message);
-		for (uint64_t i = 0; i < bytes.size() / BLOCK_LENGTH; ++i)
-		{
-			std::vector<uint32_t> schedule(16, 0);
-			for (uint32_t j = 0; j < BLOCK_LENGTH; ++j)
-			{
-				schedule[j / 4] |= (bytes[i + j]) << (j % 4 * 8);
-			}
-
-			int32_t a = state[0], b = state[1], c = state[2], d = state[3], e = state[4];
-			int32_t aa = state[0], bb = state[1], cc = state[2], dd = state[3], ee = state[4];
-			int32_t t = 0, tt = 0;
-
-			for (uint32_t j = 0; j < 80; ++j)
-			{
-				uint32_t jj = j / 16;
-				t = std::rotl(a + ff(jj + 1, b, c, d) + schedule[RL[j]] + KL[jj], SL[j]) + e;
-				tt = std::rotl(aa + ff(5 - jj, bb, cc, dd) + schedule[RR[j]] + KR[jj], SR[j]) + ee;
-
-				a = e;
-				e = d;
-				d = std::rotl(static_cast<uint32_t>(c), 10);
-				c = b;
-				b = t;
-
-				aa = ee;
-				ee = dd;
-				dd = std::rotl(static_cast<uint32_t>(cc), 10);
-				cc = bb;
-				bb = tt;
-			}
-
-			t = state[1] + c + dd;
-			state[1] = state[2] + d + ee;
-			state[2] = state[3] + e + aa;
-			state[3] = state[4] + a + bb;
-			state[4] = state[0] + b + cc;
-			state[0] = t;
-		}
-
-		std::stringstream stream;
-		for (uint32_t i = 0; i < state.size() * 4; ++i)
-		{
-			int8_t byte_value = static_cast<int8_t>(unsigned_right_shift(state[i / 4], i % 4 * 8));
-			stream << std::setfill('0') << std::setw(2) << std::hex << (byte_value & 0xff);
-		}
-
-		return stream.str();
-	}
+uint8_t ripemd160_shifts[80] =
+{ 11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8
+, 12, 13, 11, 15, 6, 9, 9, 7, 12, 15, 11, 13, 7, 8, 7, 7
+, 13, 15, 14, 11, 7, 7, 6, 8, 13, 14, 13, 12, 5, 5, 6, 9
+, 14, 11, 12, 14, 8, 6, 5, 5, 15, 12, 15, 14, 9, 9, 8, 6
+, 15, 12, 13, 13, 9, 5, 8, 6, 14, 11, 12, 11, 8, 6, 5, 5
 };
 
-//================================================================================
-// Function: RIPEMD160T
-// Description: Computes the RIPEMD160 hash of input data. Initializes an internal
-//              instance, generates a hex digest via message_digest(), and writes
-//              the binary result (20 bytes) into the provided output buffer.
-//================================================================================
-uint8_t* RIPEMD160T(const uint8_t* data, const size_t len, uint8_t* out)
+uint32_t ripemd160_constants_left[5] =
+{ 0x00000000UL, 0x5a827999UL, 0x6ed9eba1UL, 0x8f1bbcdcUL, 0xa953fd4eUL };
+
+uint32_t ripemd160_constants_right[5] =
+{ 0x50a28be6UL, 0x5c4dd124UL, 0x6d703ef3UL, 0x7a6d76e9UL, 0x00000000UL };
+
+uint8_t ripemd160_fns_left[5] = { 1, 2, 3, 4, 5 };
+
+uint8_t ripemd160_fns_right[5] = { 5, 4, 3, 2, 1 };
+
+#define ROL(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
+
+void ripemd160_compute_line(uint32_t* digest, uint32_t* words, uint32_t* chunk, uint8_t* index, uint8_t* shifts, uint32_t* ks, uint8_t* fns) {
+    for (uint8_t i = 0; i < 5; i++) {
+        words[i] = digest[i];
+    }
+
+    for (uint8_t round = 0; /* breaks out mid-loop */; round++) {
+        uint32_t k = ks[round];
+        uint8_t  fn = fns[round];
+        for (uint8_t i = 0; i < 16; i++) {
+            uint32_t tmp;
+            switch (fn) {
+            case 1:
+                tmp = words[1] ^ words[2] ^ words[3];
+                break;
+            case 2:
+                tmp = (words[1] & words[2]) | (~words[1] & words[3]);
+                break;
+            case 3:
+                tmp = (words[1] | ~words[2]) ^ words[3];
+                break;
+            case 4:
+                tmp = (words[1] & words[3]) | (words[2] & ~words[3]);
+                break;
+            case 5:
+                tmp = words[1] ^ (words[2] | ~words[3]);
+                break;
+            }
+            tmp += words[0] + chunk[index[i]] + k;
+            tmp = ROL(tmp, shifts[index[i]]) + words[4];
+            words[0] = words[4];
+            words[4] = words[3];
+            words[3] = ROL(words[2], 10);
+            words[2] = words[1];
+            words[1] = tmp;
+        }
+        if (round == 4) {
+            break;
+        }
+        shifts += 16;
+
+        uint8_t index_tmp[16];
+        for (uint8_t i = 0; i < 16; i++) {
+            index_tmp[i] = ripemd160_rho[index[i]];
+        }
+        for (uint8_t i = 0; i < 16; i++) {
+            index[i] = index_tmp[i];
+        }
+    }
+}
+
+void ripemd160_update_digest(uint32_t* digest, uint32_t* chunk)
 {
-	if (!data || !out)
-		return nullptr;
+    uint8_t index[16];
 
+    /* initial permutation for left line is the identity */
+    for (uint8_t i = 0; i < 16; i++) {
+        index[i] = i;
+    }
+    uint32_t words_left[5];
+    ripemd160_compute_line(digest, words_left, chunk, index, ripemd160_shifts, ripemd160_constants_left, ripemd160_fns_left);
 
-	std::string hex = message_digest(std::string(reinterpret_cast<const char*>(data), len));
+    /* initial permutation for right line is 5+9i (mod 16) */
+    index[0] = 5;
+    for (uint8_t i = 1; i < 16; i++) {
+        index[i] = (index[i - 1] + 9) & 0x0f;
+    }
+    uint32_t words_right[5];
+    ripemd160_compute_line(digest, words_right, chunk, index, ripemd160_shifts, ripemd160_constants_right, ripemd160_fns_right);
 
-	for (size_t i = 0; i < 20; ++i)
-	{
-		unsigned v = 0;
+    /* update digest */
+    digest[0] += words_left[1] + words_right[2];
+    digest[1] += words_left[2] + words_right[3];
+    digest[2] += words_left[3] + words_right[4];
+    digest[3] += words_left[4] + words_right[0];
+    digest[4] += words_left[0] + words_right[1];
 
-		std::stringstream ss;
-		ss << std::hex << hex.substr(i * 2, 2);
-		ss >> v;
+    /* final rotation */
+    words_left[0] = digest[0];
+    digest[0] = digest[1];
+    digest[1] = digest[2];
+    digest[2] = digest[3];
+    digest[3] = digest[4];
+    digest[4] = words_left[0];
+}
 
-		out[i] = static_cast<uint8_t>(v & 0xFF);
-	}
+void ripemd160(const uint8_t* data, uint32_t data_len, uint8_t* digest_bytes)
+{
+    /* NB assumes correct endianness */
+    uint32_t* digest = (uint32_t*)digest_bytes;
+    for (uint8_t i = 0; i < 5; i++) {
+        digest[i] = ripemd160_initial_digest[i];
+    }
 
-	return out;
+    const uint8_t* last_chunk_start = data + (data_len & (~0x3f));
+    while (data < last_chunk_start) {
+        ripemd160_update_digest(digest, (uint32_t*)data);
+        data += 0x40;
+    }
+
+    uint8_t last_chunk[0x40];
+    uint8_t leftover_size = data_len & 0x3f;
+    for (uint8_t i = 0; i < leftover_size; i++) {
+        last_chunk[i] = *data++;
+    }
+
+    /* append a single 1 bit and then zeroes, leaving 8 bytes for the length at the end */
+    last_chunk[leftover_size] = 0x80;
+    for (uint8_t i = leftover_size + 1; i < 0x40; i++) {
+        last_chunk[i] = 0;
+    }
+
+    if (leftover_size >= 0x38) {
+        /* no room for size in this chunk, add another chunk of zeroes */
+        ripemd160_update_digest(digest, (uint32_t*)last_chunk);
+        for (uint8_t i = 0; i < 0x38; i++) {
+            last_chunk[i] = 0;
+        }
+    }
+
+    uint32_t* length_lsw = (uint32_t*)(last_chunk + 0x38);
+    *length_lsw = (data_len << 3);
+    uint32_t* length_msw = (uint32_t*)(last_chunk + 0x3c);
+    *length_msw = (data_len >> 29);
+
+    ripemd160_update_digest(digest, (uint32_t*)last_chunk);
 }
--- src/btc-parser/Ripemd160.h
+++ src/btc-parser/Ripemd160.h
@@ -1,8 +1,27 @@
-#pragma once
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2021 David Turner
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 
-//================================================================================
-// Function: RIPEMD160T
-// Description: External function wrapper for computing RIPEMD-160 hash using raw 
-//              input buffer. Returns pointer to the output hash bytes.
-//================================================================================
-uint8_t* RIPEMD160T(const uint8_t* data, const size_t len, uint8_t* out);
+#include <stdint.h>
+
+void ripemd160(const uint8_t* data, uint32_t data_len, uint8_t* digest_bytes);
