09e7aed64b7f539f618c177c86f20229fbc812b2
Evgeniy <dinobunny.re@gmail.com>
2025-11-30 20:37:04 +0100

    Fixed 1-3 edits + added logs for greater clarity of errors.

--- src/btc-parser/CsvParser.cpp
+++ src/btc-parser/CsvParser.cpp
@@ -26,16 +26,19 @@ bool DownloadKaggleCsv()
 
 	std::unique_ptr<CURL, decltype(&curl_easy_cleanup)> curl{ curl_easy_init(), &curl_easy_cleanup };
 	if (!curl)
+	{
+		LOG_ERROR("Failed to initialize CURL (curl_easy_init returned nullptr)");
 		return false;
+	}
 
 
 	FILE* fp = nullptr;
 	errno_t err = fopen_s(&fp, kOutPath, "wb");
-	if (err || !fp)
+	if (err)
+	{
+		LOG_ERROR("Failed to open output file = \"{}\", errno = {}", kOutPath, err);
 		return false;
-
-	std::unique_ptr<FILE, decltype(&fclose)> file{ fp, &fclose };
-	fp = nullptr;
+	}
 
 	curl_easy_setopt(curl.get(), CURLOPT_URL, kKaggleUrl);
 	curl_easy_setopt(curl.get(), CURLOPT_FOLLOWLOCATION, 1L);
@@ -44,21 +47,23 @@ bool DownloadKaggleCsv()
 	curl_easy_setopt(curl.get(), CURLOPT_SSL_VERIFYPEER, 1L);
 	curl_easy_setopt(curl.get(), CURLOPT_SSL_VERIFYHOST, 2L);
 
-	curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, file.get());
+	curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, fp);
 
 	CURLcode res = curl_easy_perform(curl.get());
 	long http = 0;
 	curl_easy_getinfo(curl.get(), CURLINFO_RESPONSE_CODE, &http);
 
-	constexpr const errno_t ERR = 200;
+	constexpr long kHttpOk = 200;
 
-	if (res != CURLE_OK || http != ERR)
+	if (res != CURLE_OK || http != kHttpOk)
 	{
+		fclose(fp);
 		std::remove(kOutPath);
 		LOG_ERROR("curl error: {}, HTTP: {}\n", curl_easy_strerror(res), http);
 		return false;
 	}
 
+	fclose(fp);
 	return true;
 }
 
@@ -69,8 +74,6 @@ bool DownloadKaggleCsv()
 std::expected<std::map<std::string, double>, AppErrorCode> LoadCsvToMap(const std::string& path)
 
 {
-	std::map<std::string, double> result;
-
 	// Opening a CSV file
 	std::ifstream fin(path);
 	if (!fin.is_open())
@@ -78,6 +81,8 @@ std::expected<std::map<std::string, double>, AppErrorCode> LoadCsvToMap(const st
 		LOG_ERROR("Cannot open CSV file: {}\n", path);
 		return std::unexpected(AppErrorCode::FileNotFound);
 	}
+
+	std::map<std::string, double> result;
 
 	std::string line;
 	std::getline(fin, line); // Skipping the header
@@ -118,8 +123,6 @@ std::expected<std::map<std::string, double>, AppErrorCode> LoadCsvToMap(const st
 //================================================================================
 std::expected<std::map<std::wstring, double>, AppErrorCode> LoadCsvToMap(const std::wstring& path)
 {
-	std::map<std::wstring, double> result;
-
 	// Opening a CSV file
 	std::wifstream fin(path);
 	if (!fin.is_open())
@@ -127,6 +130,8 @@ std::expected<std::map<std::wstring, double>, AppErrorCode> LoadCsvToMap(const s
 		LOG_ERROR("Cannot open CSV file: {}\n", path);
 		return std::unexpected(AppErrorCode::FileNotFound);
 	}
+
+	std::map<std::wstring, double> result;
 
 	std::wstring line;
 	std::getline(fin, line); // Skipping the header
